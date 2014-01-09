915d4266d43fc757a339f176e202b555dda7dc5e
xaoonipu <irla.pawel@gmail.com>
2014-01-09 22:22:00 +0100

    Sidebar menu

--- js/main.js
+++ js/main.js
@@ -1,29 +1,37 @@
 $(function(){
 	
-	$('h3').prepend(function() { return '<a href="#' + $.trim($(this).text()) + '"><i class="glyphicon glyphicon-paperclip"></i></a> '; } );
-	$('h4').prepend(function() { return '<a href="#' + $.trim($(this).text()) + '"><i class="glyphicon glyphicon-pushpin"></i></a> '; } );
-	
-	var $sidebar = $('<ul class="nav nav-1">');
-	var $article = $('#article');
-	
-	var structure = [];
-	var prev = '';
-	var addToMenu = function($hs, level, $appendTo) {
-		if (level > 6) {
-			return;
-		}
-		if ($hs.length > 0) {
-			$.each($hs, function(){
-				var $li = $('<li>');
-				$li.append($('<a>' + $(this).text() + '</a>'));
-				$appendTo.append($li);
-				addToMenu()
-			});
-		} else {
-			var $li = $('<li>');
-			addToMenu(null, level + 1, $li);
-		}
+	var icons = {
+		H1: 'briefcase',
+		H2: 'list-alt',
+		H3: 'pushpin',
+		H4: 'paperclip',
+		H5: 'pencil',
+		H6: 'tag'
 	};
 	
-	addToMenu($article.find('h1'), 1, $sidebar);
+	var $hs = $('#article').find('h1, h2, h3, h4, h5, h6');
+	var $sidebar = $('#sidebar');
+	
+	$.each($hs, function(){
+		var $this = $(this);
+		var text = $.trim($this.text());
+		var id = text.toLowerCase().replace(/\s/g,'_');
+		$this.attr('id', id);
+		var tagName = $this.prop('tagName');
+		
+		$this.html('<a href="#' + text + '"><i class="glyphicon glyphicon-' + icons[tagName] + '"></i></a> ' + text);
+		$sidebar.append($('<li class="nav-' + tagName + '"><a href="#' + id + '">' + text + '</a></li>'));
+	});
+	
+	$sidebar.affix({
+		offset: {
+			top: 236,
+			bottom: 0
+		}
+	});
+	
+	$('body').scrollspy({ target: '#sidebar-container' });
+	
+	setTimeout(function(){$('body').scrollspy('refresh');}, 100);
+	
 });
